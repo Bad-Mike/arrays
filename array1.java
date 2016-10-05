b7a3d19927c2db8d8d1e922870b1aea8a38dffef
Bad-Mike <mike.plotek@gmx.de>
2016-10-05 12:41:02 +0200

    Create array1.java

---
+++ array1.java
@@ -0,0 +1,20 @@
+package arrays;
+
+/**
+ * Created by mike on 04/10/2016.
+ */
+public class array1 {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[5];      // array
+
+        array[0]=3;                    // declare indexposition
+        array[1]=33;
+        array[2]=45;
+        array[3]=11;
+        array[4]=6;
+
+        System.out.println("Indexpostionion 2 and 4" + array[2] + " " + array[4]);
+    }
+}
